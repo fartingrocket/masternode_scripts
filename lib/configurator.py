717311e9b3c6fdc2fd06edecf09ed458cbcb7afc
Rocket <benniou.hichem@gmail.com>
2021-01-23 18:54:47 +0100

    Configurator refactoring Better handling of params reading from params.json

--- lib/configurator.py
+++ lib/configurator.py
@@ -26,31 +26,27 @@ class configurator:
             print("\n#### Reading params.json\n")
             with open(self.params_file) as json_file:
                 loaded_params = json.load(json_file)
-            if "ticker" in loaded_params and loaded_params["ticker"] not in ({}, [], "", None):
-                self.ticker = loaded_params["ticker"]
-                print("'{}' found : {}".format("ticker", self.ticker))
-            else:
-                print("Missing param 'ticker' in the params.json")
-                self.set_ticker()
-            if "wallet_data_dir" in loaded_params and loaded_params["wallet_data_dir"] not in ({}, [], "", None):
-                self.wallet_data_dir = loaded_params["wallet_data_dir"]
-                print("'{}' found : {}".format("wallet_data_dir", self.wallet_data_dir))
-            if "wallet_cli_path" in loaded_params and loaded_params["wallet_cli_path"] not in ({}, [], "", None):
-                self.wallet_cli_path = loaded_params["wallet_cli_path"]
-                print("'{}' found : {}".format("wallet_cli_path", self.wallet_cli_path))
+            for param_name, value in loaded_params.items():
+                if value not in ({}, [], "", None):
+                    if param_name == "headers" and \
+                            (value in ({}, [], "", None) or value["IHOSTMN-API-KEY"] in ({}, [], "", None)):
+                        print("'{}' value empty in the params.json".format(param_name))
+                        self.set_headers()
+                    else:
+                        setattr(self, param_name, value)
+                        print("'{}' found : {}".format(param_name, value))
+                # We ignore "wallet_data_dir" and "wallet_cli_path" as these are used for wallet_handle
+                # If they are set however, they will be used in chain checks
+                elif param_name not in ["wallet_data_dir", "wallet_cli_path"]:
+                    print("'{}' value empty in the params.json".format(param_name))
+                    call = "set_" + param_name
+                    getattr(self, call)()
+                else:
+                    print("'{}' value empty in the params.json".format(param_name))
+
             if self.wallet_data_dir and self.wallet_cli_path:
                 self.wallet_handle = wallet(data_dir=self.wallet_data_dir, cli_path=self.wallet_cli_path)
-            if "headers" in loaded_params and loaded_params["headers"] not in ({}, [], "", None):
-                self.headers = loaded_params["headers"]
-                print("'{}' found : {}".format("headers", self.headers))
-            else:
-                print("Missing param 'headers' in the params.json. Attempting to retrieve.")
-                self.set_header()
-            if "new_txs" in loaded_params and loaded_params["new_txs"] not in ({}, [], "", None):
-                self.new_txs = loaded_params["new_txs"]
-                print("'{}' found : {}".format("new_txs", self.new_txs))
-            else:
-                print("Missing param 'new_txs' in the params.json")
+
             self.save_params_json()
         else:
             print("#### Missing or empty params.json file starting configuration mode\n")
@@ -60,7 +56,7 @@ class configurator:
     def prompt_params_creation(self):
 
         self.set_ticker()
-        self.set_header()
+        self.set_headers()
         if prompt_confirmation("Do you want to use the wallet interface to set transactions ?", default="y"):
             self.set_new_txs()
         else:
@@ -69,15 +65,11 @@ class configurator:
 
     def save_params_json(self, reload=False):
 
-        # As during load the whole headers param is stored in self.headers a check is needed
-        # to avoid writing another dict inside the existing one
-        _api_key = self.headers["IHOSTMN-API-KEY"] if "IHOSTMN-API-KEY" in self.headers else self.headers
-
         params = {
             "ticker": self.ticker,
+            "headers": self.headers,
             "wallet_data_dir": self.wallet_data_dir,
             "wallet_cli_path": self.wallet_cli_path,
-            "headers": {"IHOSTMN-API-KEY": _api_key},
             "new_txs": self.new_txs
         }
 
@@ -95,15 +87,15 @@ class configurator:
             self.ticker = input("Input ticker (ex.: SAPP): ").upper()
         return self.ticker
 
-    def set_header(self):
+    def set_headers(self):
         # IHOSTMN-API-KEY
-        while not self.headers:
-            self.headers = input("Input your IHOSTMN-API-KEY : ")
-        return self.headers
+        while not self.headers or self.headers["IHOSTMN-API-KEY"] in ({}, [], "", None):
+            self.headers = {"IHOSTMN-API-KEY": input("Input your IHOSTMN-API-KEY : ")}
 
     def set_new_txs(self):
         # Transactions list for MN creation
-        # These 2 'If' conditions need to be separate. We first check for handle then we check connection with wallet
+        # These 'If' conditions need to be nested.
+        # We first check for handle then we check connection with wallet
         if self.wallet_handle:
             if self.wallet_handle.check_server():
                 self.new_txs = json.loads(self.wallet_handle.get_masternode_outputs())
